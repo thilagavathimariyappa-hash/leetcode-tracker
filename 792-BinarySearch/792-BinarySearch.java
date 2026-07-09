aa24536fa40659f67131c3ea5811c19dbd4b9fcd
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-09 10:28:32 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,25 @@
+// Last updated: 7/8/2026, 10:31:15 PM
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
