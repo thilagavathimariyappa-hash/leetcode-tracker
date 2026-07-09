74d7efcb364cdcaa4c4e3d723a6bed0d3cf2ebdc
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-09 10:28:36 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,15 @@
+// Last updated: 7/8/2026, 10:31:19 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a,b);
+    }
+}
