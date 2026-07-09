166a2fce7eb05934d2df7599de534392f112b067
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-09 10:29:03 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,16 @@
+// Last updated: 7/8/2026, 10:31:47 PM
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        int i = 0;
+
+        for(int j=1;j<nums.length;j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }  
+        }
+        return i+1;
+    }
+}
