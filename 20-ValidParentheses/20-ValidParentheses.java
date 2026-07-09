76dd3c2fd85c4aa3b50992b7cf645097d0551384
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-09 10:29:07 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,33 @@
+// Last updated: 7/8/2026, 10:31:51 PM
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(')
+                    return false;
+
+                if (ch == '}' && top != '{')
+                    return false;
+
+                if (ch == ']' && top != '[')
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
