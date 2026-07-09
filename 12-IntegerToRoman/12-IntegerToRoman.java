8ac095ed5f4bb6dbe20b6923d26e925f188ca241
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-09 10:29:09 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.java
@@ -0,0 +1,28 @@
+// Last updated: 7/8/2026, 10:31:53 PM
+class Solution {
+    public String intToRoman(int num) {
+
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return ans.toString();
+    }
+}
