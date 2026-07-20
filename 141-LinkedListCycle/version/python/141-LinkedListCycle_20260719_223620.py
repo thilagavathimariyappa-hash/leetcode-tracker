8e3d3e0aa7934e7ab321c26c0b89337e850ff28b
thilagavathimariyappa-hash <thilagavathimariyappa@gmail.com>
2026-07-20 10:33:29 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/version/python/141-LinkedListCycle_20260719_223620.py
@@ -0,0 +1,14 @@
+# Last updated: 7/19/2026, 10:36:20 PM
+1class Solution:
+2    def hasCycle(self, head):
+3        slow = head
+4        fast = head
+5
+6        while fast and fast.next:
+7            slow = slow.next
+8            fast = fast.next.next
+9
+10            if slow == fast:
+11                return True
+12
+13        return False
